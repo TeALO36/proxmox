e4ff56fab12929d0a5413a52ed2d7673dc88da9f
TeALO36 <83779026+TeALO36@users.noreply.github.com>
2022-07-15 14:34:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 exec bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install
-
+exec bash <(curl -s https://raw.githubusercontent.com/Tontonjo/proxmox_toolbox/main/proxmox_toolbox.sh )
